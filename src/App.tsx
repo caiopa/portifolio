6dc934ff42fed325bc49b003505c0f67d029d572
Caio Pereira Antunes <caioefg@gmail.com>
2023-03-02 11:26:42 -0300

    Iniciando Portifolio e imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Perfil from './pages/perfil';
 
 function App() {
   return (
-    <div className="App">
-      ops
-    </div>
+    <main className="vw-100 vh-100 bg-dark text-white">
+      <Perfil />
+    </main>
   );
 }
 
