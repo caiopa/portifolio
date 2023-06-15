b89efb61fb592593cff55e3977b106b43bff801c
Caio Pereira <caioefg@gmail.com>
2023-06-15 10:01:18 -0300

    Fix

--- src/components/sobre/sobre.tsx
+++ src/components/sobre/sobre.tsx
@@ -12,7 +12,7 @@ class SobreMim extends React.Component {
             <h3 id="sobreMim" className="a text-center mb-5 fs-2" data-aos="fade-up" data-aos-duration="1000">Sobre mim</h3>
               <div className='fs-5'>
                 <p className="text-justify lh-base" data-aos="fade-up">
-                Tenho 29 anos sou de Volta Redonda - Rj, sou formado em Engenharia Civil e Desenvolvedor Web Full-Stack, possuo habilidades em back-end, front-end e banco de dados, tenho experiencia anteriores em manutenção elétrica industrial, residencial e engenharia civil, como fiscalização de obra e projetos, um pouco dos meus hobbies é, viajar, ver filmes, séries, comer, jogar, codar, estar com amigos e família.
+                Tenho 29 anos sou de Volta Redonda - Rj, formado em Engenharia Civil e Desenvolvedor Web Full-Stack, possuo habilidades em back-end, front-end e banco de dados, tenho experiencia anteriores em manutenção elétrica industrial, residencial e engenharia civil, como fiscalização de obra e projetos, um pouco dos meus hobbies é, viajar, ver filmes, séries, comer, jogar, codar, estar com amigos e família.
                 </p>
                 <p className="text-justify lh-base" data-aos="fade-up">
                   Meu objetivo é utilizar minhas habilidades para criar soluções eficientes e de alta qualidade. Sou apaixonado por tecnologia e estou sempre buscando aprender mais para aprimorar meus conhecimentos e desenvolver soluções inovadoras.
