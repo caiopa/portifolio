7697a646e43732da5ff51ee8ea1da0d40ff5bc04
Caio Pereira <caioefg@gmail.com>
2023-07-04 09:14:52 -0300

    Style: colocando barControll on scroll pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Perfil from './pages/perfil';
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const progressBar = document.querySelector('.scroll-progress') as HTMLElement;
+      const maxScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+      
+      if (progressBar) {
+        const currentScroll = window.pageYOffset;
+        const scrollProgress = (currentScroll / maxScrollHeight) * 100;
+
+        progressBar.style.width = `${scrollProgress}%`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <main className="vh-100 text-white">
+      <div className="scroll-progress"></div>
       <Perfil />
     </main>
   );
