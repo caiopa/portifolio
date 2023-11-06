8476538695e69e5b2739d5fd72de8f62ff60401b
Caio Antunes <caioefg@gmail.com>
2023-11-06 09:40:22 -0300

    incluindo projeto cpastore no portifolio e atualizando pagina sobre

--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import deliveryImg from '../../images/projeto-delivery.png';
 import receitasImg from '../../images/projeto-receitas.jpg';
-import triviaImg from '../../images/projeto-trivia.png';
+import ecommerce from '../../images/ecommerce.png';
 import githubImg from '../../images/GitHub2.jpg'
 import musicIcon from '../../images/icon3.png';
 import { motion } from "framer-motion";
@@ -14,6 +14,23 @@ class Projetos extends React.Component {
         <section id="projetos" className="text-center w-75 m-auto mt-5">
           <h3 data-aos="fade-up" className="my-5 a fs-2">Projetos</h3>
           <div className="d-flex flex-column w-75 m-auto ">
+          <Nav.Link href="https://app-full-stack-pi.vercel.app" target="_blank">
+            <motion.div className="card mb-3 bg-dark fs-5 border-0" whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }} >
+               <div className="row no-gutters bg-dark"  data-aos="fade-right"    >
+                 <div className="col-md-4 m-auto">
+                   <img src={ecommerce} className="card-img img-fluid  same-size-img" alt="..."/>
+                 </div>
+                 <div className="col-md-8">
+                   <div className="card-body">
+                     <h5 className="card-title">CPA Store</h5>
+                     <p className="card-text">Loja de e-commerce</p>
+                     <p className='a'> Tecnologias: Reactjs, Nextjs, NextAuth, Typescript, Tailwindcss, CSS, Context, Stripe, Nodejs, Prisma, Supabase </p>
+                   </div>
+                 </div>
+               </div>
+            </motion.div>
+          </Nav.Link>
           <Nav.Link href="https://github.com/caiopa/AppDelivery" target="_blank" >
             <motion.div className="card mb-3 bg-dark border-0 fs-5" whileHover={{ scale: 1.05 }}
               transition={{ type: "spring", stiffness: 400, damping: 10 }} >
@@ -49,24 +66,8 @@ class Projetos extends React.Component {
                </div>
             </motion.div>
           </Nav.Link>
-          <Nav.Link href="https://github.com/caiopa/ProjetoGrupoTrivia" target="_blank">
-            <motion.div className="card mb-3 bg-dark fs-5 border-0" whileHover={{ scale: 1.05 }}
-            transition={{ type: "spring", stiffness: 400, damping: 10 }} >
-               <div className="row no-gutters bg-dark"  data-aos="fade-right"    >
-                 <div className="col-md-4 m-auto">
-                   <img src={triviaImg} className="card-img img-fluid  same-size-img" alt="..."/>
-                 </div>
-                 <div className="col-md-8">
-                   <div className="card-body">
-                     <h5 className="card-title">Trivia Quiz</h5>
-                     <p className="card-text">Jogo de perguntas e respostas baseado no Show do milhão, com o objetivo de acessar uma API externa</p>
-                     <p className='a'> Tecnologias: Reactjs, JS, CSS, Context </p>
-                   </div>
-                 </div>
-               </div>
-            </motion.div>
-          </Nav.Link>
-          <Nav.Link href="https://github.com/caiopa/beatMusic" target="_blank">
+          
+          <Nav.Link href="https://beat-music.vercel.app/" target="_blank">
             <motion.div className="card mb-3 bg-dark fs-5 border-0" whileHover={{ scale: 1.05 }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}>
                <div className="row no-gutters bg-dark" data-aos="fade-left">
